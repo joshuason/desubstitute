ee87bff7490f384e7da3e0ef22ecf87e13556de8
Joshua Son <joshuason@live.com>
2020-03-24 17:29:12 +1300

    Worked on edit functions of textarea

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 const Textarea = ({value, title, onChange}) => {
 
   const [highlight, setHighlight] = useState({chars: null, isValid: false});
+  const [casper, setCasper] = useState({char: null, selection: null});
   const highlightDiv = useRef(null);
 
   useEffect(() => {
@@ -11,15 +12,74 @@ const Textarea = ({value, title, onChange}) => {
     // }
   }, [highlight]);
 
+  // Sets highlight
   const handleSelect = e => {
+
     const {selectionStart, selectionEnd} = e.target;
     const selected = value.substring(selectionStart, selectionEnd);
     console.log(selectionStart, selectionEnd, selected);
     setHighlight({chars: selected, isValid: (selected.length !== 0)});
   }
 
+  // Syncs the highlight div to the textarea
+  const handleScroll = e => {
+    var scrollTop = e.target.scrollTop;
+    highlightDiv.current.scrollTop = scrollTop;
+  }
+
+  // Resets highlight on blur
+  const handleBlur = e => {
+    setHighlight({chars: null, isValid: false});
+    setCasper({char: null, selection: null});
+    e.target.setSelectionRange(0,0);
+  }
+
+  const handleChange = e => {
+    const { value } = e.target;
+    onChange(value);
+    // Allow single entry edits
+    // Not multiple eg highlight multiple and edit or pasting
+
+  }
+
+  const handleKeyDown = e => {
+    if (e.key === 'Backspace') {
+      if (highlight.chars) {
+        return;
+      }
+      const {selectionStart, selectionEnd} = e.target;
+      if (casper.char) {
+        e.target.setSelectionRange(selectionStart+1, selectionEnd+1);
+        setCasper({char: null, selection: null});
+        return;
+      }
+      e.preventDefault();
+      console.log(e.key);
+      const char = (selectionStart === selectionEnd)
+      ? value.substring(selectionStart, selectionEnd-1)
+      : null;
+      if (char) {
+        const selection = [selectionStart-1, selectionEnd];
+        setCasper({char, selection});
+        console.log(char, selection);
+      }
+      (selectionStart !== 0) && e.target.setSelectionRange(selectionStart-1, selectionEnd-1);
+    }
+  }
+
+/* // If highlighted, paste may replace one char
+  const handlePaste = e => {
+    e.preventDefault();
+    return false;
+  }
+*/
+  //onClick
+  //onMouseOver
+
+/* // Returns an array of strings split into fragments
+  before and after highlighted fragment for rendering highlights */
   const highlighted = () => {
-    let ta = value;
+    let ta = value; // text array
     let arr = [];
     let i = 0;
     while (highlight.isValid && ta.includes(highlight.chars)) {
@@ -33,39 +93,6 @@ const Textarea = ({value, title, onChange}) => {
     return arr;
   }
 
-  const handleScroll = (e) => {
-    var scrollTop = e.target.scrollTop;
-    //console.log(scrollTop);
-    highlightDiv.current.scrollTop = scrollTop;
-  }
-
-  const handleChange = (e) => {
-    // Allow single entry edits
-    // Not multiple eg highlight multiple and edit or pasting
-
-  }
-  //onPaste -- disabled
-  //onClick
-  //onMouseOver
-
-
-  return (
-    <div id="Testbox" style={testboxStyle}>
-      {title}
-      <div className="container" style={containerStyle}>
-        {
-          (highlight.isValid)
-          ? highlighted().map((word, index) => {
-              if (word === highlight.chars)
-                return <span key={word+index} style={{backgroundColor: "yellow"}}>{word}</span>
-              return word
-            })
-          : value
-        }
-      </div>
-    </div>
-  );
-  /*
   return (
     <div id="Textarea">
     {title}
@@ -79,25 +106,33 @@ const Textarea = ({value, title, onChange}) => {
             (highlight.isValid)
             ? highlighted().map((word, index) => {
                 if (word === highlight.chars)
-                  return <span key={word+index} style={{backgroundColor: "yellow"}}>{word}</span>
+                  return <span key={word+index} style={highlightStyle}>{word}</span>
                 return word
               })
-            : value
+            : (casper.char)
+              ? value.split('').map((char, index) =>
+                  (char === casper.char)
+                  ? <span key={char+index} style={casperStyle}>{char}</span>
+                  : char
+                )
+              : value
           }
         </div>
         <textarea
           value={value}
           style={textareaStyle}
-          onChange={e => onChange(e.target.value)}
+          onChange={e => handleChange(e)}
           onClick={e => handleSelect(e)}
-          onBlur={() => setHighlight({chars: null, isValid: false})}
-          onScroll={(e) => handleScroll(e)}
+          onBlur={e => handleBlur(e)}
+          onScroll={e => handleScroll(e)}
+          onKeyDown={e => handleKeyDown(e)}
+          //onPaste={e => handlePaste(e)}
         />
       </div>
     </div>
-  );*/
+  );
 }
-/*
+
 const textboxStyle = {
   position: "relative",
   top: "0",
@@ -114,7 +149,8 @@ const textboxStyle = {
   overflowX: "hidden",
   whiteSpace: "pre-wrap",
   wordWrap: "break-word",
-  color: "rgba(255, 255, 255, 0)",
+  color: "black",
+  letterSpacing: "0.1rem"
 }
 
 const textareaStyle = {
@@ -130,20 +166,23 @@ const textareaStyle = {
   fontFamily: "courier",
   fontSize: "13px",
   backgroundColor: "rgba(255, 255, 255, 0)",
+  color: "rgba(255, 255, 255, 0)",
   cursor: "text",
-}*/
-
-const testboxStyle = {
-  display: "block",
-  margin: "50px 0 0 0"
+  letterSpacing: "0.1rem",
 }
 
-const containerStyle = {
-  width: "300px",
-  height: "150px",
-  borderRadius: "0",
-  border: "1px solid black",
-  textAlign: "left",
+const highlightStyle = {
+  backgroundColor: "yellow"
 }
+
+const casperStyle = {
+  color: "grey",
+  backgroundColor: "lightgrey",
+  boxSizing: "border-box",
+  margin: 0,
+  padding: 0,
+}
+
+
 
 export default Textarea;
