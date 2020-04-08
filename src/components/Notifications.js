69ec9607557ffe25cd390eb9c59aff8567ee021d
Joshua Son <joshuason@live.com>
2020-04-08 17:20:00 +1200

    Refactored Notifications

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,106 +1,45 @@
 import React from 'react';
+import data from '../data/frequencies'
 
-const cornell_letterFrequency = {
-  e: 0.1202,
-  t: 0.0910,
-  a: 0.0812,
-  o: 0.0768,
-  i: 0.0731,
-  n: 0.0695,
-  s: 0.0628,
-  r: 0.0602,
-  h: 0.0593,
-  d: 0.0432,
-  l: 0.0398,
-  u: 0.0288,
-  c: 0.0271,
-  m: 0.0261,
-  f: 0.0230,
-  y: 0.0211,
-  w: 0.0209,
-  g: 0.0203,
-  p: 0.0182,
-  b: 0.0149,
-  v: 0.0111,
-  k: 0.0069,
-  x: 0.0017,
-  q: 0.0011,
-  j: 0.0010,
-  z: 0.0007,
-}
-const cornell_bigramFrequency = {
-  th: 0.0152,
-  he: 0.0128,
-  in: 0.0094,
-  er: 0.0094,
-  an: 0.0082,
-  re: 0.0068,
-  nd: 0.0063,
-  at: 0.0059,
-  on: 0.0057,
-  nt: 0.0056,
-  ha: 0.0056,
-  es: 0.0056,
-  st: 0.0055,
-  en: 0.0055,
-  ed: 0.0053,
-  to: 0.0052,
-  it: 0.0050,
-  ou: 0.0050,
-  ea: 0.0047,
-  hi: 0.0046,
-  is: 0.0046,
-  or: 0.0043,
-  ti: 0.0034,
-  as: 0.0033,
-  te: 0.0027,
-  et: 0.0019,
-  ng: 0.0018,
-  of: 0.0016,
-  al: 0.0009,
-  de: 0.0009,
-  se: 0.0008,
-  le: 0.0008,
-  sa: 0.0006,
-  si: 0.0005,
-  ar: 0.0004,
-  ve: 0.0004,
-  ra: 0.0004,
-  ld: 0.0002,
-  ur: 0.0002,
-}
+const {
+  // // Use Cornell for whitespace dependent
+  // cornell_monogram,
+  // cornell_bigram,
+  // Use PC for whitespace independent
+  pc_monogram,
+  pc_bigram,
+  pc_trigram
+} = data;
 
 const Notifications = ({charAnalysis}) => {
   const { monograms, bigrams, trigrams } = charAnalysis;
 
-  const monogramAnalysis = analyseInput(cornell_letterFrequency, monograms);
-  const bigramAnalysis = analyseInput(cornell_bigramFrequency, bigrams);
-  // const trigramAnalysis = analyseInput(controldata, trigrams);
+  // Could refactor below as a custom hook [useAnalyseInput(charAnalysis)]
+  const monogramAnalysis = analyseInput(pc_monogram, monograms);
+  const bigramAnalysis = analyseInput(pc_bigram, bigrams);
+  const trigramAnalysis = analyseInput(pc_trigram, trigrams);
 
-  // const inputAnalyses = { monogramAnalysis, bigramAnalysis, trigramAnalysis };
+  const inputAnalyses = { monogramAnalysis, bigramAnalysis, trigramAnalysis };
   // const inputInterpretation = interpret(inputAnalyses);
   // const outputAnalysis = {};
 
-  /*
-    // sorted human readable percentages (ie Strings);
-    const unigramPercentages = getPercentagesOfObj(monograms);
-    const bigramPercentages = getPercentagesOfObj(bigrams);
-  */
+  // sorted human readable percentages (ie Strings);
+  const monogramPercentages = getPercentagesOfObj(monograms);
+  const bigramPercentages = getPercentagesOfObj(bigrams);
+  const trigramPercentages = getPercentagesOfObj(trigrams);
+
+  const inputPercentages = { monogramPercentages, bigramPercentages, trigramPercentages};
+
+  console.log('Raw', inputPercentages);
+  console.log('Churned', inputAnalyses);
+
   return (
     <div id="Notifications">
-      {/*
-        (unigramPercentages)
-        && unigramPercentages.map(item => {
-          return <p>{item[0]}: {item[1]}</p>
-        })
-        */
+      {
+
       }
     </div>
   );
-}
-
-function objToArray(obj) {
-  return Object.entries(obj);
 }
 
 function objToSortedArray(obj) {
@@ -112,44 +51,50 @@ function numToPercentage(num) {
 }
 
 function getPercentagesOfObj(obj) {
-  return objToArray(obj).sort((a, b) => b[1] - a[1]).map(item =>
+  return objToSortedArray(obj).map(item =>
     [item[0], numToPercentage(item[1])]
   );
 }
 
-function getDeviation(control, variable) {
-  return (control - variable) ** 2;
+function getDeviation(control, sample) {
+  return (control - sample) ** 2;
 }
 
-function analyseInput(control, data) {
-  const control_sortedArray = objToSortedArray(control);
-  const data_sortedArray = objToSortedArray(data);
+function analyseInput(control, sample) {
+  const controlSortedArray = objToSortedArray(control);
+  const sampleSortedArray = objToSortedArray(sample);
 
-  if (!control_sortedArray || !data_sortedArray) {
+  if (!controlSortedArray || !sampleSortedArray) {
     return null;
   }
 
-  const dataLength = data_sortedArray.length;
-  const lowerBound = 5; // Also cast_size ::end
-  const upperBound = dataLength - lowerBound; // ::begin
-  const mid = Math.floor(lowerBound / 2);
-  const deviation = data_sortedArray.reduce((acc, cur, ind, arr) => {
-    const begin = (ind - mid > 0)
-      ? (ind - mid < upperBound)
-        ? ind - mid
-        : upperBound
+  const { length } = controlSortedArray;
+  const sampleSlice = sampleSortedArray.slice(0, length);
+  const radius = 2;
+  const diameter = 2 * radius;
+  const deviation = sampleSlice.reduce((acc, cur, ind, arr) => {
+    const begin = (ind - radius > 0)
+      ? (ind - radius < length - diameter)
+        ? ind - radius
+        : length - diameter - 1
       : 0;
-    const end = (ind + mid + 1 < dataLength)
-      ? (ind + mid + 1 > lowerBound)
-        ? ind + mid + 1
-        : lowerBound
-      : dataLength;
+    const end = (ind + radius < length)
+      ? (ind + radius > diameter)
+        ? ind + radius + 1
+        : diameter + 1
+      : length;
 
-    const subControl =  control_sortedArray.slice(begin, end);
-    const deviationArray = subControl.map(item =>
-      [item[0], getDeviation(cur[1], item[1])]
+    const controlSlice = controlSortedArray.slice(begin, end);
+    const deviationArray = controlSlice.map(item =>
+      [item[0], getDeviation(item[1], cur[1])]
     ).sort((a, b) => a[1] - b[1]);
-    acc[cur[0]] = deviationArray;
+
+    if (deviationArray.length) {
+      acc[cur[0]] = deviationArray;
+    } else {
+      console.log('Something went wrong in deviationArray');
+    }
+
     return acc;
   }, {});
 
