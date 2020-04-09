70a30df49598860d88bfbc5cee5d594e728f9b87
Joshua Son <joshuason@live.com>
2020-04-09 13:09:16 +1200

    Added helper functions to notifications

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -20,7 +20,7 @@ const Notifications = ({charAnalysis}) => {
   const trigramAnalysis = analyseInput(pc_trigram, trigrams);
 
   const inputAnalyses = { monogramAnalysis, bigramAnalysis, trigramAnalysis };
-  // const inputInterpretation = interpret(inputAnalyses);
+  // const inputInterpretation = interpretInput(inputAnalyses);
   // const outputAnalysis = {};
 
   // sorted human readable percentages (ie Strings);
@@ -30,8 +30,11 @@ const Notifications = ({charAnalysis}) => {
 
   const inputPercentages = { monogramPercentages, bigramPercentages, trigramPercentages};
 
-  console.log('Raw', inputPercentages);
+  console.log('Raw', charAnalysis);
+  console.log('Raw (-h)', inputPercentages);
   console.log('Churned', inputAnalyses);
+  console.log('Inverted', invertAnalysisObj(getInversePercentagesOfAnalysis(monogramAnalysis)));
+  console.log('Inverted', getInversePercentagesOfAnalysis(trigramAnalysis));
 
   return (
     <div id="Notifications">
@@ -40,6 +43,18 @@ const Notifications = ({charAnalysis}) => {
       }
     </div>
   );
+}
+
+function objToArray(obj) {
+  return Object.entries(obj);
+}
+
+function arrayToObj(arr) {
+  return Object.fromEntries(arr);
+}
+
+function getKeysFromObj(obj) {
+  return Object.keys(obj);
 }
 
 function objToSortedArray(obj) {
@@ -56,11 +71,42 @@ function getPercentagesOfObj(obj) {
   );
 }
 
+function invertAnalysisObj(obj) {
+  const arr = objToArray(obj);
+
+  const inverse = arr.reduce((acc, cur, ind, arr) => {
+    const product = cur[1].map(metaItem => [metaItem[0], [cur[0], metaItem[1]]]);
+    product.forEach(element => {
+      // console.log(element)
+      acc = {
+        ...acc,
+        [element[0]]: (acc[element[0]]) ? [[element[1][0], element[1][1]],
+          ...acc[element[0]]] : [[element[1][0], element[1][1]]],
+      };
+    });
+    return acc;
+  }, {});
+
+  return inverse;
+}
+
+function getInversePercentagesOfAnalysis(obj) {
+  const keys = getKeysFromObj(obj);
+  const newObj = keys.reduce((acc, key, ind, arr) => {
+    acc = {
+      ...acc,
+      [key]: obj[key].map(item => [item[0], 1 - item[1]]),
+    };
+    return acc;
+  }, {});
+  return newObj;
+}
+
 function getDeviation(control, sample) {
   return (control - sample) ** 2;
 }
 
-function analyseInput(control, sample) {
+function analyseInput(control, sample, radius = 2) {
   const controlSortedArray = objToSortedArray(control);
   const sampleSortedArray = objToSortedArray(sample);
 
@@ -70,7 +116,7 @@ function analyseInput(control, sample) {
 
   const { length } = controlSortedArray;
   const sampleSlice = sampleSortedArray.slice(0, length);
-  const radius = 2;
+  //const radius = 2;
   const diameter = 2 * radius;
   const deviation = sampleSlice.reduce((acc, cur, ind, arr) => {
     const begin = (ind - radius > 0)
@@ -99,6 +145,15 @@ function analyseInput(control, sample) {
   }, {});
 
   return deviation;
+}
+
+function interpretInput(analyses, occurences) {
+  const { monogramAnalysis } = analyses;
+  const { monograms } = occurences;
+
+  const interpretation = {};
+
+  return interpretation;
 }
 
 export default Notifications;
