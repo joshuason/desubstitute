0ad79ede17c3eb516b8e7c002f49fb263a2d3d26
Joshua Son <joshuason@live.com>
2020-04-22 16:13:53 +1200

    Add autosolve function

--- src/components/Autosolver.js
+++ src/components/Autosolver.js
@@ -3,7 +3,8 @@ import React from 'react';
 const alphabetArray = [...Array(26).keys()].map(num => String.fromCharCode(num+97));
 
 const Autosolver = ({input, cipherKey}) => {
-  generateRandomParentKey();
+
+  autosolve(input);
   return (
     <div className="Autosolver">
       <button onClick={(input, cipherKey) => autosolve(input, cipherKey)} />
@@ -12,7 +13,49 @@ const Autosolver = ({input, cipherKey}) => {
 }
 
 function autosolve(input, cipherKey) {
-  generateRandomParentKey();
+  // if (!cipherKey) {
+    hillClimbingAlgorithm(input);
+  // } else {
+  //   myAlgorithm(input, cipherKey);
+  // }
+}
+
+function hillClimbingAlgorithm(input) {
+  let parent = generateRandomParentKey();
+  let fitness = measureFitness(decipher(input, parent));
+  for (let i = 0; i < 10; i++) {
+    const newParentKey = changeKeySlightly(parent);
+    const newFitness = measureFitness(decipher(input, newParentKey));
+    if (newFitness > fitness) {
+      parent = newParentKey;
+      fitness = newFitness;
+      i = 0;
+    }
+  }
+}
+
+function decipher(input, key) {
+  return input.toLowerCase().split('').map(char => key[char]).join('');
+}
+
+function measureFitness(decipheredText) {
+
+}
+
+function changeKeySlightly(key) {
+  const genRandom = () => Math.floor(Math.random() * alphabetArray.length);
+  let randomPair = [genRandom(), genRandom()];
+  while (randomPair[0] === randomPair[1]) {
+    randomPair = [genRandom(), genRandom()];
+  }
+  const keysToSwap = Object.keys(key).filter((keyItem, ind) => randomPair.includes(ind));
+  const swap = key[keysToSwap[0]];
+  const newKey = {
+    ...key,
+    [keysToSwap[0]]: key[keysToSwap[1]],
+    [keysToSwap[1]]: swap,
+  };
+  return newKey;
 }
 
 function generateRandomParentKey() {
